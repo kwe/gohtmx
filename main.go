2a213b4af2a272e086aed3be34da954084b5571a
Kevin Evans <kwevans@gmail.com>
2023-06-17 17:21:34 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/helmet"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/template/html"
+)
+
+func main() {
+	engine := html.New("./views/templates", ".html")
+	config := fiber.Config{
+		Views:       engine,
+		ViewsLayout: "layouts/main",
+		Prefork:     false,
+	}
+
+	app := fiber.New(config)
+	app.Use(helmet.New())
+	app.Use(recover.New())
+	app.Use(logger.New())
+
+	app.Static("/static", "static")
+	app.Get("/", func(c *fiber.Ctx) error {
+		// Render index template
+		return c.Render("index", fiber.Map{
+			"Title": "Hello, World!",
+		})
+	})
+	app.Get("/metrics", monitor.New())
+
+	log.Fatal(app.Listen(":3000"))
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+```bash
+npx tailwindcss -i ./input.css -o ./static/style.css --watch
+```
